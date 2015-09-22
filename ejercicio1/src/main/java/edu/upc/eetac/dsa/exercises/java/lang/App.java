fdb36f8c6717aa1001ef4e1452ec880146186d6d
jtardio <jesus.tardio.lopez@gmail.com>
2015-09-22 12:55:55 +0200

    Primera subida de ejercicio 1

---
+++ ejercicio1/src/main/java/edu/upc/eetac/dsa/exercises/java/lang/App.java
@@ -0,0 +1,24 @@
+package edu.upc.eetac.dsa.exercises.java.lang;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Tree[] arboles = new Tree[4];
+        arboles[0] = new Tree(4);
+        arboles[1] = new Tree("Roble");
+        arboles[2] = new Tree();
+        arboles[3] = new Tree(5, "Pino");
+
+
+
+        for (Tree t : arboles)
+            System.out.println(t);
+
+    }
+}
+
---
+++ ejercicio1/src/main/java/edu/upc/eetac/dsa/exercises/java/lang/README.md
@@ -0,0 +1,6 @@
+Ejercicio 1
+
+Constructores de clase Tree
+
+Genera diferentes objetos sobre una clase creada por nosotros desde el inicio con parámetros definidos
+
